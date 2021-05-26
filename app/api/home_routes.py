449ca604eb945e31e71d6828bdb48bbc83c9c9b2
Hieu-Ma <simshieu1@gmail.com>
2021-05-26 15:16:55 -0700

    working on discovery post

--- app/api/home_routes.py
+++ app/api/home_routes.py
@@ -1,10 +1,13 @@
-from flask import Blueprint
-from app.models import Server, db
+from flask import Blueprint, session
+from app.models import Server, db, user_server
 
 home_routes = Blueprint('home', __name__)
 
 @home_routes.route('/')
 def servers():
    servers = Server.query.all()
-   print('TESTING THE QUERY WITH SERVERS', servers)
+   # print('TESTING THE QUERY WITH SERVERS', user_server)
    return {"servers": [server.to_dict() for server in servers]}
+
+# @home_route.route('/', methods=['POST'])
+# def createUserServer(userId, serverId):

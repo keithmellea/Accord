d2810455856858657850bd06aee6bc0c16d199ff
Hieu-Ma <simshieu1@gmail.com>
2021-05-27 16:26:11 -0700

    modal now closes after creating server

--- react-app/src/components/Navbar/Navbar.js
+++ react-app/src/components/Navbar/Navbar.js
@@ -1,16 +1,20 @@
 import React, {useState} from 'react';
-import { NavLink } from 'react-router-dom';
+import { NavLink, useHistory } from 'react-router-dom';
 // import LogoutButton from '../auth/LogoutButton';
 import { useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
-import { getUsersServers } from "../../store/servers";
 import { ContextMenuTrigger } from "react-contextmenu";
+import { getUsersServers } from "../../store/servers";
 import { allServersByUserId } from "../../store/user_server";
+// import {joinServer} from "../../store/discover";
 import { addServer } from "../../store/servers";
 import Modal from '@material-ui/core/Modal';
 import './Navbar.css'
 
 const NavBar = () => {
+
+  let history = useHistory()
+
   const dispatch = useDispatch();
   const userId = useSelector((state) => state.session.user?.id)
   const usersServers = useSelector((state) => {return state.user_server.server;});
@@ -40,7 +44,12 @@ const NavBar = () => {
     console.log(server_name)
     console.log(img_url)
     dispatch(addServer(img_url, server_name))
+    setOpen(false)
   }
+
+  // function redirect() {
+  //   setOpen(false)
+  // }
 
   return (
     <nav className="navbar">
